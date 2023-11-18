25a471e7b3077578a576775aa1de9bfc44fdee0f
GorbunovIvan <gorbunovvivan@gmail.com>
2023-11-18 09:02:59 +0200

    init

---
+++ src/main/java/com/example/SysGearsSpringJson4TasksApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysGearsSpringJson4TasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysGearsSpringJson4TasksApplication.class, args);
+    }
+
+}
